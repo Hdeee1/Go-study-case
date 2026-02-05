23902192101227fe03271dd4970d5036a9110f24
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2026-02-05 09:38:53 +0700

    first commit

---
+++ case1/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const dbConnString = "user:password@tcp(127.0.0.1:3306)/db_study_case_na"
+const dbMaxIdleConn = 4
+const dbMaxConns = 100
+const countWorker = 100
+const csvFile = "majestic_million.csv"
+var dataHeder = make([]string, 0)
+
+func dbConnection() (*sql.DB, error) {
+	log.Println("=> open db connection")
+
+	db, err := sql.Open("mysql", dbConnString)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(dbMaxConns)
+	db.SetMaxIdleConns(dbMaxIdleConn)
+
+	return db, nil
+}
+
+func openCsvFile() (*csv.Reader, *os.File, error) {
+	log.Println("=> open csv file")
+
+	f, err := os.Open(csvFile)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	reader := csv.NewReader(f)
+	return reader, f, nil
+}
+
+func dispatchWorkers(db *sql.DB, jobs <-chan []interface{}, wg *sync.WaitGroup) {
+	for workerIndex := 0; workerIndex < countWorker; workerIndex++ {
+		go func(workerIndex int, db *sql.DB, job <-chan []interface{}, wg *sync.WaitGroup) {
+			counter := 0
+
+			for job := range jobs {
+				doTheJob(workerIndex, counter, db, job)
+				wg.Done()
+				counter++
+			}
+		}(workerIndex, db, jobs, wg)
+	}
+}
+
+func readCsvFilePerLineThenSendToWorker(csv *csv.Reader, jobs chan<- []interface{}, wg *sync.WaitGroup) {
+	for {
+		row, err := csv.Read()
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			break
+		}
+
+		if len(dataHeder) == 0 {
+			dataHeder = row
+			continue
+		}
+
+		rowOrdered := make([]interface{}, 0)
+		for _, each := range row {
+			rowOrdered = append(rowOrdered, each)
+		}
+
+		wg.Add(1)
+		jobs <-rowOrdered
+	}
+	close(jobs)
+}
+
+func doTheJob(workerIndex, counter int, db *sql.DB, values []interface{}) {
+	for {
+		var outerError error
+		func(outerError *error) {
+			defer func() {
+				if err := recover(); err != nil {
+					*outerError = fmt.Errorf("%v", err)
+				}
+			}()
+
+			conn, err := db.Conn(context.Background())
+			query := fmt.Sprintf("INSERT INTO domain (%s) VALUES (%s)", 
+				strings.Join(dataHeder, ","),
+				strings.Join(generateQuestionMark(len(dataHeder)), ","),
+			)
+
+			_, err = db.ExecContext(context.Background(), query, values...)
+			if err != nil {
+				log.Printf("worker %d error: %v", workerIndex, err)
+			}
+
+			err = conn.Close()
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}(&outerError)
+		if outerError == nil {
+			break
+		}
+	}
+
+	if counter%100 == 0 {
+		log.Println("=> worker", workerIndex, "inserted", counter, "data")
+	}
+}
+
+func generateQuestionMark(n int) []string {
+	s := make([]string, 0)
+	for i := 0; i < n; i++ {
+		s = append(s, "?")
+	}
+	return s
+}
+
+func main() {
+	start := time.Now()
+
+	db, err := dbConnection()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	csvReader, csvFile, err := openCsvFile()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer csvFile.Close()
+
+	jobs := make(chan []interface{}, 100)
+	wg := new(sync.WaitGroup)
+
+	dispatchWorkers(db, jobs, wg)
+	readCsvFilePerLineThenSendToWorker(csvReader, jobs, wg)
+
+	wg.Wait()
+
+	duration := time.Since(start)
+	fmt.Println("done in", int(math.Ceil(duration.Seconds())), "second")
+}
